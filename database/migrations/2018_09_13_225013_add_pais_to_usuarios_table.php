70a6c5ca7823ae27d58546985b18e25e12342bee
Rodrigo <rodrigodrupal1@gmail.com>
2018-09-13 18:02:40 -0500

    Migrations uno

---
+++ database/migrations/2018_09_13_225013_add_pais_to_usuarios_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPaisToUsuariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('usuarios', function (Blueprint $table) {
+
+            $table->string('pais')->nullable()->after('email');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('usuarios', function (Blueprint $table) {
+            $table->dropColumn('pais');
+        });
+    }
+}
